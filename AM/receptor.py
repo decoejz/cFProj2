4f12a83860538ebfc52520726a11e821ba19f884
decoejz <decoejz@gmail.com>
2018-10-26 12:04:50 -0300

    atualizano

--- AM/receptor.py
+++ AM/receptor.py
@@ -15,7 +15,6 @@ looping = True
 
 # Frequência e duração
 fs = 44100
-cont = np.linspace(0, 88200, 88200)
 amplitude = 1
 freqP = 14000
 duration = 5  # seconds
@@ -29,28 +28,31 @@ sd.default.samplerate = fs
 #while looping:
 
 #Gravação de sinal
+print('Vai começar a gravar')
 dirtySignal = sd.rec(int(duration * fs))
 sd.wait()
+print('Acabou de gravar')
 #time.sleep(1)
 
 #limpa o sinal -- correção do erro da placa de audio
-
 dirtySignal = dirtySignal[:,0]
-#demod = dirtySignal[:,0]
+
+#Demodulando o sinal
+
 demod = dirtySignal*Port #np.multiply(dirtySignal,Port)
 demod = passaBaixa(demod,fs)
 
 #Objeto da classe para calculo da FFT
 sig = signalMeu()
-freq, t = sig.calcFFT(dirtySignal,fs)
-print("frequencias:     ",t)
+freqS, tS = sig.calcFFT(dirtySignal,fs)
+print("frequencias:     ",tS)
 
 #Encontrando a posição dos picos
-indexes = peakutils.indexes(t, thres=3, min_dist=70)
+indexesS = peakutils.indexes(tS, thres=3, min_dist=70)
 
 #Picos - lista
-peaks_x = peakutils.interpolate(freq,t, ind=indexes)
-print("picos:           ", peaks_x)
+peaks_xS = peakutils.interpolate(freqS,tS, ind=indexesS)
+print("picos:           ", peaks_xS)
 
 #for p in freq:
 	#if abs(13000 - p) < 200:
@@ -66,17 +68,27 @@ peaks_x = peakutils.interpolate(freq,t, ind=indexes)
 print("picos:           ", peaks_x)
 
 #Gráfico do sinal sonoro recebido
-plt.plot(demod)
+plt.plot(dirtySignal)
 plt.title("Sinal recebido (tempo)")
 plt.show()
 
 #Plota o gráfico com as frequências e os picos 
+pplot(freqS, tS, indexesS)
+plt.title('Sinal recebido (Frequências)')
+plt.show()
+
+#Gráfico do sinal sonoro recebido
+plt.plot(demod)
+plt.title("Sinal demodulado (tempo)")
+plt.show()
+
+#Plota o gráfico com as frequências e os picos 
 pplot(freq, t, indexes)
-plt.title('Frequências')
+plt.title('Sinal demodulado (Frequências)')
 plt.show()
 
 print("começou a emitir")
-sd.play(demod*100)
+sd.play(demod)
 sd.wait()
 print("terminou a emissão")
 #break
